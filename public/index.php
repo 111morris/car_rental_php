1c4cb8848aa69ce2d93a6d8f5e083e42b48cf80c
mulandi <mulandimorris1@gmail.com>
2025-12-01 17:24:01 +0300

    implement initial car rental application including core logic, routing, rendering, database services, and UI components.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,27 @@
 <?php
 
-require_once('../classes/Router.php');
+use App\Core\App;
+use App\Core\Session;
 
-Router::route();
+// Simple Autoloader
+spl_autoload_register(function ($class) {
+    $prefix = 'App\\';
+    $base_dir = __DIR__ . '/../app/';
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+Session::start();
+
+$app = new App();
+$app->run();
